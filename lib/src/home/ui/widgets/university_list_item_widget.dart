80b9ae40b24c1dd3b4b69f5cb193af1929b3fbc9
Martin Alejandro Escobar Espinel <martinale4@hotmail.com>
2022-11-07 16:51:16 -0500

    adding detail screen

--- lib/src/home/ui/widgets/university_list_item_widget.dart
+++ lib/src/home/ui/widgets/university_list_item_widget.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:stacked_hooks/stacked_hooks.dart';
+import 'package:universities/core/extensions/university_extension.dart';
 import 'package:universities/core/utils/app_colors.dart';
 import 'package:universities/core/widgets/app_text.dart';
 import 'package:universities/models/university_model.dart';
@@ -16,12 +19,27 @@ class UniversityListItemWidget extends HookViewModelWidget<HomeViewModel> {
   @override
   Widget buildViewModelWidget(BuildContext context, HomeViewModel viewModel) =>
       ListTile(
-        leading: const Icon(
-          Icons.school,
-          color: AppColors.primary,
-          size: 50,
-        ),
-        onTap: () {},
+        leading: UniversityExtension.images[university.name] != null
+            ? SizedBox(
+                width: 80,
+                height: 80,
+                child: Center(
+                  child: Image.file(
+                    File(
+                      UniversityExtension.images[university.name]!.path,
+                    ),
+                    fit: BoxFit.cover,
+                    width: 50,
+                    height: 50,
+                  ),
+                ),
+              )
+            : const Icon(
+                Icons.school,
+                color: AppColors.primary,
+                size: 50,
+              ),
+        onTap: () => viewModel.goTodetail(university),
         title: AppText(
           university.name,
           fontWeigth: AppFontWeigth.semibold,
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import devicelocale
 import shared_preferences_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
